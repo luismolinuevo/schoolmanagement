d7c6e10abb640ea8c8db7b9ca2a03572bc7d42fd
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-12-05 00:57:46 -0500

    mysql connection working

--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const mysql = require("mysql");
+dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-// Create a MySQL connection
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "your_username",
-//   password: "your_password",
-//   database: "your_database",
-// });
+//Create a MySQL connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
-// // Connect to the database
-// connection.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to database:", err);
-//     return;
-//   }
-//   console.log("Connected to MySQL database");
-// });
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
 
-const PORT = 3000;
+//Start the express server
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
